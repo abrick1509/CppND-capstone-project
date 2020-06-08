5903b73cca9c033d5a43e12a51a63399cd3b3fcd
alex <abrickwedde@gmail.com>
2020-06-08 21:35:46 +0200

    added print out for winner

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
+#include <algorithm>
 #include <iostream>
+#include <string>
 
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
@@ -24,12 +26,24 @@ int main() {
   game.Run(controller, renderer, kMsPerFrame);
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Number of Players: " << game.GetNumberOfPlayers() << "\n";
+  std::vector<std::pair<std::string, int>> scores;
   for (std::size_t i = 0; i < game.GetNumberOfPlayers(); ++i) {
     const auto this_snake = game.GetSnake(i);
     std::cout << "Player " << i << ":\n";
     std::cout << "\tScore: \t\t" << this_snake.GetScore() << std::endl;
     std::cout << "\tSnake size: \t" << this_snake.GetSize() << std::endl;
     std::cout << "\tSnake alive: \t" << this_snake.alive << std::endl;
+    // sort scores of all players that survived
+    if (this_snake.alive) {
+      scores.push_back({"Player " + std::to_string(i), this_snake.GetScore()});
+    }
   }
+  std::sort(scores.begin(), scores.end(), [](const auto &lhs, const auto &rhs) {
+    return lhs.second < rhs.second;
+  });
+  const auto &winner = scores.back();
+  std::cout << "The winner is: " << winner.first
+            << ", with a score of: " << winner.second << std::endl;
+
   return 0;
 }
