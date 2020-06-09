f4d329a93167065a9884681ae42356141b62a5ff
alex <abrickwedde@gmail.com>
2020-06-09 10:51:15 +0200

    increased window and grid size

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr std::size_t kScreenWidth{640};
-  constexpr std::size_t kScreenHeight{640};
-  constexpr std::size_t kGridWidth{32};
-  constexpr std::size_t kGridHeight{32};
+  constexpr std::size_t kScreenWidth{800};
+  constexpr std::size_t kScreenHeight{800};
+  constexpr std::size_t kGridWidth{50};
+  constexpr std::size_t kGridHeight{50};
 
   Renderer renderer(kScreenWidth,  //
                     kScreenHeight, //
